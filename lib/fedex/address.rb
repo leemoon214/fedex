1b49044e8d4e6a9b50e607c397fe8fbd32be1f29
moon <leemoon214@gmail.com>
2017-03-07 22:16:04 -0500

    Stop using parse_response in Address because of parsing issue, exception occurred

--- lib/fedex/address.rb
+++ lib/fedex/address.rb
@@ -6,24 +6,24 @@ module Fedex
                 :province_code, :postal_code, :country_code
 
     def initialize(options)
-      @changes   = options[:changes]
-      @score     = options[:score].to_i
-      @confirmed = options[:delivery_point_validation] == "CONFIRMED"
-      @available = options[:delivery_point_validation] != "UNAVAILABLE"
+      @changes   = options["Changes"]
+      @score     = options["Score"].to_i
+      @confirmed = options["DeliveryPointValidation"] == "CONFIRMED"
+      @available = options["DeliveryPointValidation"] != "UNAVAILABLE"
 
       @status      = options[:residential_status]
       @residential = status == "RESIDENTIAL"
       @business    = status == "BUSINESS"
 
-      address        = options[:address]
+      address        = options["Address"]
 
-      @company       = options[:company_name]
-      @street_lines  = address[:street_lines]
-      @city          = address[:city]
-      @state         = address[:state_or_province_code]
-      @province_code = address[:state_or_province_code]
-      @postal_code   = address[:postal_code]
-      @country_code  = address[:country_code]
+      @company       = options["CompanyName"]
+      @street_lines  = address["StreetLines"]
+      @city          = address["City"]
+      @state         = address["StateOrProvinceCode"]
+      @province_code = address["StateOrProvinceCode"]
+      @postal_code   = address["PostalCode"]
+      @country_code  = address["CountryCode"]
 
       @options = options
     end
--- lib/fedex/request/address.rb
+++ lib/fedex/request/address.rb
@@ -16,14 +16,15 @@ module Fedex
       def process_request
         api_response = self.class.post(api_url, :body => build_xml)
         puts api_response if @debug == true
-        response = parse_response(api_response)
+        #response = parse_response(api_response)
+        response = api_response
         if success?(response)
-          options = response[:address_validation_reply][:address_results][:proposed_address_details]
+          options = response["AddressValidationReply"]["AddressResults"]["ProposedAddressDetails"]
           options = options.first if options.is_a? Array
           Fedex::Address.new(options)
         else
-          error_message = if response[:address_validation_reply]
-            [response[:address_validation_reply][:notifications]].flatten.first[:message]
+          error_message = if response["AddressValidationReply"]
+            [response["AddressValidationReply"]["Notifications"]].flatten.first["Message"]
           else
             "#{api_response["Fault"]["detail"]["fault"]["reason"]}\n--#{api_response["Fault"]["detail"]["fault"]["details"]["ValidationFailureDetail"]["message"].join("\n--")}"
           end rescue $1
@@ -88,8 +89,8 @@ module Fedex
 
       # Successful request
       def success?(response)
-        response[:address_validation_reply] &&
-          %w{SUCCESS WARNING NOTE}.include?(response[:address_validation_reply][:highest_severity])
+        response["AddressValidationReply"] &&
+          %w{SUCCESS WARNING NOTE}.include?(response["AddressValidationReply"]["HighestSeverity"])
       end
 
     end
